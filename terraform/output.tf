cfd84cd4dd5fb9efd647c314be884dde657e2de3
Pritam Bhavsar <pritambhavsar@gmail.com>
2023-05-18 12:35:29 +0530

    fix typo in out variable

--- terraform/output.tf
+++ terraform/output.tf
@@ -6,6 +6,6 @@ output "public_ip_address" {
   value = azurerm_linux_virtual_machine.mediawiki_vm.public_ip_address
 }
 
-output local_file" {
+output "local_file" {
   value     = local_file.linux_key.filename
 }
